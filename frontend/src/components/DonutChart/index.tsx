3dc9c35051d876ae9b2e4c54032beec2d3c70050
akmazurok <ak.mazurok@gmail.com>
2021-05-08 17:58:25 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -10,32 +11,52 @@ type ChartData = {
 
 const DonutChart = () => {
     
-    //forma errada
-    let chartData : ChartData = { labels: [], series: []};
+    //
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: []});
 
-    /*forma errada - para testar se esta chamando os dados da API
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
+
+                setChartData({ labels: myLabels, series: mySeries });
+            });
+
+    }, []);
+
+    
+    // 2 - FORMA ERRADA
+    //let chartData : ChartData = { labels: [], series: []};
+
+    
+    /* 3- FORMA ERRADA - para testar se esta chamando os dados da API
     //usa crase pra montar essa url
+    
     axios.get(`${BASE_URL}/sales/amount-by-seller`)
         .then( response => {
             console.log(response.data);
 
         }); */
-        
+    
+    /* 4 - FORMA ERRADA
+    //dessa forma os dados ficam na variavel mas nao chama o grafico pq essa chamada eh assincrona
     axios.get(`${BASE_URL}/sales/amount-by-seller`)
         .then( response => {
             const data = response.data as SaleSum[];
             const myLabels = data.map(x => x.sellerName);
             const mySeries = data.map(x => x.sum);
 
-            chartData = { labels: myLabels, series: mySeries};
+            //chartData = { labels: myLabels, series: mySeries};
+            setChartData({labels: myLabels, series: mySeries}});
 
             console.log(chartData);
 
-        });
+        }); */
     
-
-
-    /* DADOS ESTATICOS - usados na construcao do frontend
+    
+    /* 1 - DADOS ESTATICOS - usados na construcao do frontend
     const mockData = { 
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
